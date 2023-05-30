9bb251d33b62ba9b58021f41a2040ba95bf5b25e
Tushar Chavan <tusharrchavan014@gmail.com>
2023-05-31 01:33:00 +0530

    Fix bugs

--- main.py
+++ main.py
@@ -1,59 +1,215 @@
-import numpy as np
-from sklearn.preprocessing import StandardScaler
 import pickle
 import streamlit as st
-
-loaded_model = pickle.load(open("models/parkinsons.sav", 'rb'))
-
-def parkinsons(input_data):
-    # changing input data to a numpy array
-  input_data_as_numpy_array = np.asarray(input_data)
-
-  # reshape the numpy array
-  input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-
-  prediction = loaded_model.predict(input_data_reshaped)
-  print(prediction)
+from streamlit_option_menu import option_menu
 
 
-  if (prediction[0] == 0):
-    return "The Person does not have Parkinsons Disease"
-  else:
-    return "The Person has Parkinsons"
+parkinsons_model = pickle.load(open('/home/tushar/Dev/Projects/diagnosis-of-chronic-diseases-using-machine-learning/models/parkinsons.sav', 'rb'))
+diabetes_model = pickle.load(open('/home/tushar/Dev/Projects/diagnosis-of-chronic-diseases-using-machine-learning/models/diabetes.sav', 'rb'))
+heart_disease_model = pickle.load(open('/home/tushar/Dev/Projects/diagnosis-of-chronic-diseases-using-machine-learning/models/heart.sav','rb'))
+
+
+with st.sidebar:
+    selected = option_menu('Diagnosis of Chronic Diseases using ML',
+                          ['Diabetes',
+                            'Heart Disease',
+                            'Parkinsons Disease'],
+                          icons=['activity','heart','person'],
+                          default_index=0)
+
+
+
+if (selected == "Parkinsons Disease"):
+    
+    st.title("Diagnose Parkinson's Disease")
+    
+    col1, col2, col3, col4, col5 = st.columns(5)  
+    
+    with col1:
+        fo = st.text_input('MDVP:Fo(Hz)')
+        
+    with col2:
+        fhi = st.text_input('MDVP:Fhi(Hz)')
+        
+    with col3:
+        flo = st.text_input('MDVP:Flo(Hz)')
+        
+    with col4:
+        Jitter_percent = st.text_input('MDVP:Jitter(%)')
+        
+    with col5:
+        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
+        
+    with col1:
+        RAP = st.text_input('MDVP:RAP')
+        
+    with col2:
+        PPQ = st.text_input('MDVP:PPQ')
+        
+    with col3:
+        DDP = st.text_input('Jitter:DDP')
+        
+    with col4:
+        Shimmer = st.text_input('MDVP:Shimmer')
+        
+    with col5:
+        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
+        
+    with col1:
+        APQ3 = st.text_input('Shimmer:APQ3')
+        
+    with col2:
+        APQ5 = st.text_input('Shimmer:APQ5')
+        
+    with col3:
+        APQ = st.text_input('MDVP:APQ')
+        
+    with col4:
+        DDA = st.text_input('Shimmer:DDA')
+        
+    with col5:
+        NHR = st.text_input('NHR')
+        
+    with col1:
+        HNR = st.text_input('HNR')
+        
+    with col2:
+        RPDE = st.text_input('RPDE')
+        
+    with col3:
+        DFA = st.text_input('DFA')
+        
+    with col4:
+        spread1 = st.text_input('spread1')
+        
+    with col5:
+        spread2 = st.text_input('spread2')
+        
+    with col1:
+        D2 = st.text_input('D2')
+        
+    with col2:
+        PPE = st.text_input('PPE')
+        
+    
+    
+    parkinsons_diagnosis = ''
+    
+    if st.button("Test Result"):
+        parkinsons_prediction = parkinsons_model.predict([[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ,DDP,Shimmer,Shimmer_dB,APQ3,APQ5,APQ,DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE]])                          
+        
+        if (parkinsons_prediction[0] == 1):
+          parkinsons_diagnosis = "The person has Parkinson's disease"
+        else:
+          parkinsons_diagnosis = "The person does not have Parkinson's disease"
+        
+    st.success(parkinsons_diagnosis)
+
+
+
+if (selected == 'Diabetes'):
+    
+    st.title('Diagnose Diabetes')
+    
+    
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        Pregnancies = st.text_input('Number of Pregnancies')
+        
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+    
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure value')
+    
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness value')
+    
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+    
+    with col3:
+        BMI = st.text_input('BMI value')
+    
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    
+    with col2:
+        Age = st.text_input('Age of the Person')
+    
+    
+    diab_diagnosis = ''
+    
+    
+    if st.button('Test Result'):
+        diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        
+        if (diab_prediction[0] == 1):
+          diab_diagnosis = 'The person is diabetic'
+        else:
+          diab_diagnosis = 'The person is not diabetic'
+        
+    st.success(diab_diagnosis)
+
+
+
+if (selected == 'Heart Disease'):
   
-
-def main():
-  st.title("Parkinson's Disease Prediction using Machine Learning")
-
-  fo = st.text_input('MDVP:Fo(Hz)')
-  fhi = st.text_input('MDVP:Fhi(Hz)')
-  flo = st.text_input('MDVP:Flo(Hz)')
-  Jitter_percent = st.text_input('MDVP:Jitter(%)')
-  Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
-  RAP = st.text_input('MDVP:RAP')
-  PPQ = st.text_input('MDVP:PPQ')
-  DDP = st.text_input('Jitter:DDP')
-  Shimmer = st.text_input('MDVP:Shimmer')
-  Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
-  APQ3 = st.text_input('Shimmer:APQ3')
-  APQ5 = st.text_input('Shimmer:APQ5')
-  APQ = st.text_input('MDVP:APQ')
-  DDA = st.text_input('Shimmer:DDA')
-  NHR = st.text_input('NHR')
-  HNR = st.text_input('HNR')
-  RPDE = st.text_input('RPDE')
-  DFA = st.text_input('DFA')
-  spread1 = st.text_input('spread1')
-  spread2 = st.text_input('spread2')
-  D2 = st.text_input('D2')
-  PPE = st.text_input('PPE')
-
-  diagnosis = ''
-  if st.button('result'):
-    diagnosis = parkinsons([fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ,DDP,Shimmer,Shimmer_dB,APQ3,APQ5,APQ,DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE])
-
-  st.success(diagnosis)
-
-
-if __name__ == '__main__':
-  main()
+  st.title('Diagnose Heart Disease')
+  
+  col1, col2, col3 = st.columns(3)
+  
+  with col1:
+      age = st.text_input('Age')
+      
+  with col2:
+      sex = st.text_input('Sex')
+      
+  with col3:
+      cp = st.text_input('Chest Pain types')
+      
+  with col1:
+      trestbps = st.text_input('Resting Blood Pressure')
+      
+  with col2:
+      chol = st.text_input('Serum Cholestoral in mg/dl')
+      
+  with col3:
+      fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+      
+  with col1:
+      restecg = st.text_input('Resting Electrocardiographic results')
+      
+  with col2:
+      thalach = st.text_input('Maximum Heart Rate achieved')
+      
+  with col3:
+      exang = st.text_input('Exercise Induced Angina')
+      
+  with col1:
+      oldpeak = st.text_input('ST depression induced by exercise')
+      
+  with col2:
+      slope = st.text_input('Slope of the peak exercise ST segment')
+      
+  with col3:
+      ca = st.text_input('Major vessels colored by flourosopy')
+      
+  with col1:
+      thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+      
+      
+    
+    
+  heart_diagnosis = ''
+  
+  
+  if st.button('Test Result'):
+      heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg,thalach,exang,oldpeak,slope,ca,thal]])                          
+      
+      if (heart_prediction[0] == 1):
+        heart_diagnosis = 'The person is having heart disease'
+      else:
+        heart_diagnosis = 'The person does not have any heart disease'
+      
+  st.success(heart_diagnosis)
